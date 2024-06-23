409ad7ada6a4c93f706d79a32ffffee588ea1dcb
gscamurra <giovanni.scamurra.adobe@gmail.com>
2024-06-23 22:14:04 +0200

    Update youtube.js

--- blocks/youtube/youtube.js
+++ blocks/youtube/youtube.js
@@ -3,7 +3,10 @@ export default function decorate(block) {
    const [youtubeWrapper] = block.children;
    const blockyoutube = document.createElement(blockyoutube);
 
-   console.log(blockyoutube);
+   link = youtubeWrapper.getElementsByTagName('a')[0].getAttribute('href');
+
+
+   console.log(link);
 
 
 }
