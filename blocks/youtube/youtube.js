c6ad7b5ff8dc00567b8dce28fdbb1c86e5defab6
gscamurra <giovanni.scamurra.adobe@gmail.com>
2024-06-23 15:43:19 +0200

    Update youtube.js

--- blocks/youtube/youtube.js
+++ blocks/youtube/youtube.js
@@ -1,8 +1,3 @@
 export default function decorate(block) {
-    const [youtubeWrapper] = block.children;
-    const blockyoutube = document.createElement("blockyoutube");
-    
-    blockyoutube.textContent = youtubeWrapper.textContent.trim();
-    
-    youtubeWrapper = "Ironman";
+   console.log("youtube");
 }
