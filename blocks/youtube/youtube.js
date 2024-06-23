e4c82fc7dd6623ded3d5b7ded9ab0d2fcbbb5694
gscamurra <giovanni.scamurra.adobe@gmail.com>
2024-06-23 22:24:01 +0200

    Update youtube.js

--- blocks/youtube/youtube.js
+++ blocks/youtube/youtube.js
@@ -3,7 +3,9 @@ export default function decorate(block) {
    const [youtubeWrapper] = block.children;
    const link = youtubeWrapper.getElementsByTagName('a')[0].getAttribute('href');
 
-   youtubeWrapper.replaceChildren(link);
+   const iframe ='<iframe width="100%" height="315" src="'+link+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+
+   youtubeWrapper.replaceChildren(iframe);
 
    console.log(link);
 
